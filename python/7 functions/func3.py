79a04d4fa389787b0d2b80375323a8694b81c38d
AlishbaFatimaa <145777181+AlishbaFatimaa@users.noreply.github.com>
2023-10-19 22:45:18 +0500

    Add files via upload

---
+++ python/7 functions/func3.py
@@ -0,0 +1,5 @@
+def function(name):
+    print("hello" + name + "!")
+
+name = input("whats your name") 
+function(name)
---
+++ python/7 functions/func4.py
@@ -0,0 +1,7 @@
+def function(name = 'alishba'):
+    print("hello " + name + "!")
+
+function()
+name = input("whats your name")
+function(name)
+
